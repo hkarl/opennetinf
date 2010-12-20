82f2dd6942b0917331bc0d21f12f06d0570b45ac
Frederic Beister <Frederic Beister@localhost>
2010-12-20 19:36:52 +0100

    adds management tool identifier database

--- build/build.sh
+++ build/build.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
 pushd "`dirname $0`"
-if [ ! -f ~/.netinf/settings ]; then
+if [ ! -d ~/.netinf ]; then
 	mkdir ~/.netinf
+fi
+
+if [ ! -f ~/.netinf/settings ]; then
 	cp settings ~/.netinf
 fi
+
 ant
 cd dist/scripts
 ./configure.sh
+
+source ~/.netinf/settings
+
+if [ ! -L ~/netinf/ManagementToolIdentifiers ]; then
+	ln -s ${DP_LOC}/ManagementToolIdentifiers ~/.netinf/ManagementToolIdentifiers
+fi
+
 popd
