aee0256dbc3bf87723675314d5a6ec1374d83564
PG3 <PG3@localhost>
2011-02-09 11:27:39 +1300

    checkin search

---
+++ src/netinf.common/src-test/netinf/common/search/DefinedQueryTemplatesTest.java
@@ -0,0 +1,30 @@
+package netinf.common.search;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class DefinedQueryTemplatesTest {
+	
+	@Test
+	public void testSearch(){
+		DefinedQueryTemplates def = DefinedQueryTemplates.getDefinedQueryTemplateName("positionBasedShopInRadiusHasProduct");
+		assertEquals("positionBasedShopInRadiusHasProduct",DefinedQueryTemplates.getDefinedQueryTemplateName("positionBasedShopInRadiusHasProduct").getQueryTemplateName());
+		
+		assertEquals("Latitude",def.getParameters()[0][0]);
+		
+		/*String parameters[][] = new String[][] {
+		         { "Latitude", "java.lang.Double" }, { "Longitude", "java.lang.Double" }, { "Radius", "java.lang.Integer" },
+		         { "ProductsSemicolonSeparated", "java.lang.String" } };
+	
+		
+		/*for(int i=0 ; i<def.getParameters().length;i++){
+			assertEquals("Latitude",def.getParameters()[0][0]);
+			//assertEquals(parameters[i][1].toString(),def.getParameters()[i][1].toString());
+		}
+		
+		assertEquals(null,DefinedQueryTemplates.getDefinedQueryTemplateName("positionBasedShopInRadiusHasProduct"));
+		assertEquals("positionBasedShopInRadiusHasProduct",def.getQueryTemplateName());
+		*/
+	}
+}
