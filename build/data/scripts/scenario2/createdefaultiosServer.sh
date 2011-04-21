7b5ea493badc1be77f0623fb036dd4af77f03ec2
Frederic Beister <Frederic Beister@localhost>
2011-04-21 14:58:59 +0200

    making it possible to create IOs with CreateDefaultIOs

--- build/data/scripts/scenario2/createdefaultiosServer.sh
+++ build/data/scripts/scenario2/createdefaultiosServer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source ~/.netinf/settings
 sed -i "s|^cc.tcp.port = .*$|cc.tcp.port = ${SC2_PO_RS}|g" "../../configs/createIOs.properties" &> /dev/null
 cd ${DP_LOC}/jar &> /dev/null
 java -jar createdefaultios.jar &> /dev/null
