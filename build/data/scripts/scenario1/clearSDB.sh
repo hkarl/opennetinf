2e1440f560ac83a1864294715cdd01c29fa1a042
Frederic Beister <Frederic Beister@localhost>
2011-04-21 16:03:16 +0200

    decided to create new identities for demo purposes only

--- build/data/scripts/scenario1/clearSDB.sh
+++ build/data/scripts/scenario1/clearSDB.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source ~/.netinf/settings
-#/usr/bin/mysql -u root -p${DB_ROOTPW} < ${DP_LOC}/sql/netinf_node_data.sql &> /dev/null
+/usr/bin/mysql -u root -p${DB_ROOTPW} < ${DP_LOC}/sql/netinf_node_data.sql &> /dev/null
