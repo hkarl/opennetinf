80d6ea49c48444a60c561a3952a28b2130471cd5
PG3 <PG3@localhost>
2011-05-31 10:46:57 +0200

    Updated RSMDHTAck

--- src/netinf.common/src/netinf/common/messages/RSMDHTAck.java
+++ src/netinf.common/src/netinf/common/messages/RSMDHTAck.java
@@ -1,7 +1,7 @@
 package netinf.common.messages;
 
 /**
- * Confirms the storage of an IO in the uppermost ring of an MDHT hierarchy
+ * Confirms the storage of an IO in the uppermost ring of an MDHT hierarchy.
  * 
  * @author PG NetInf3, University of Paderborn
  */
