3aebf22c668b411df20a008a4c1bd654bab15d19
PG3 <PG3@localhost>
2011-06-22 21:52:51 +0200

    Chunking(basic classes and functions)

---
+++ src/netinf.node/src/netinf/node/chunking/Unit.java
@@ -0,0 +1,12 @@
+package netinf.node.chunking;
+
+/**
+ * Different units.
+ * 
+ * @author PG NetInf 3, University of Paderborn
+ */
+
+public enum Unit {
+
+   BIT, BYTE, KILOBYTE, MEGABYTE, GIGABYTE, TERABYTE
+}
